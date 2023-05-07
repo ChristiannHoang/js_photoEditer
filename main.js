0e8f331db612eeaa9234231c0b70fadfcfed86f1
Hoang Nhan <hnhan793@gmail.com>
2023-05-07 10:42:48 +0700

    done

--- main.js
+++ main.js
@@ -1,18 +1,3 @@
-/**
- * ĐẦU VÀO: 
- * CÁC GIÁ TRỊ TỪ THẺ INPUT
- * URL HÌNH ẢNH
- * GIÁ TRỊ THỬ RANGE
- * 
- * XỬ LÍ
- * GÁN VÀO FILLTER
- * TẠO HÀM GÁN CÁC GIÁ TRỊ VÀO FILTER
- * EVENTLISTENER THÌ GỌI HÀM
- * 
- * ĐẦU RA
- * FILLTER: HUE() CONTRAST() ....
- */
-
 const brightness = document.getElementById('brightness');
 const contrast = document.getElementById('contrast');
 const hue = document.getElementById('hue');
@@ -31,21 +16,28 @@ function setDefaults() {
     saturation.value = 100;
     sepia.value = 0;
     imgContent.src = 'https://cdn.trangcongnghe.vn/uploads/posts/2023-04/1680669216146.webp';
+    editorList.forEach(function(editorItem) {
+        changeRangeInput(editorItem);
+    });
+}
+
+function changeRangeInput(element) {
+    element.style.backgroundSize = `${element.value / (element.max - element.min) * 100}% 100%`;
 }
 
 setDefaults();
 
-
-document.querySelector('.submit__btn').addEventListener('click', function(e) {
+document.querySelector('.submit__btn').addEventListener('click', function (e) {
     e.preventDefault();
     setDefaults();
     imgContent.src = imgUrl.value;
 })
 
 for (var i = 0; i < editorList.length; i++) {
-    editorList[i].addEventListener('input', function(e) {
+    editorList[i].addEventListener('input', function (e) {
         e.preventDefault();
         imgContent.style.filter = `blur(${imgBlur.value}px) brightness(${brightness.value}%) contrast(${contrast.value}%) sepia(${sepia.value}%) hue-rotate(${hue.value}deg) saturate(${saturation.value}%)`;
-        console.log(brightness.value);
+        changeRangeInput(this);
     })
 }
+
